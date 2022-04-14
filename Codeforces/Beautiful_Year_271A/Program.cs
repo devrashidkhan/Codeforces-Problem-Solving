b55d7564a7e2e955e1f56be89bc3c46dee1e647c
Rashedul Islam <rashidkhan71011@gmail.com>
2022-04-14 11:24:32 +0600

    Beautiful_Year_271A Problem Solve.

---
+++ Codeforces/Beautiful_Year_271A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beautiful_Year_271A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                y += 1;
+                int a = y / 1000;
+                int b = y / 100 % 10;
+                int c = y / 10 % 10;
+                int d = y % 10;
+                if (a != b && a != c && a != d && b != c && b != d && c != d)
+                {
+                    break;
+                }
+            }
+            Console.Write(y);
+        }
+    }
+}
